9201595598c9be0515a7cc655cdbfe37d6f701f4
mmueller1369 <mm1369@email.uni-freiburg.de>
2025-05-07 20:49:16 +0200

    Updated draft of generalization

--- Exercise Sheet 02/code/params.py
+++ Exercise Sheet 02/code/params.py
@@ -1,10 +1,24 @@
-# global units, properties etc.
+# Simulation parameters
 ## Number of particles in the simulation
 nparticles = 10
 ## Time step size measured in the units of the simulation
-dt = 0
+dt = 1
 ## Amount of time steps executed of the simulation
-dt_max = 0
+dt_max = 100
+## Integrator algorithm used in the simulation; options: "velocity_verlet", "verlet", "euler"
+integrator = "velocity_verlet"
+## Potential used in the simulation; options: "lj", "gravitational"
+potential = "lj"
+## Thermostat used in the simulation; options: "none", "berendsen", "nose-hoover"
+thermostat = "none"
+## Temperature of the system in the units of the simulation
+T = 1
+## Time step for thermostat updates
+dt_thermostat = 1
+## Simulation box bounds
+box_bounds = ((0, 10), (0, 10), (0, 10))
+## Boundary conditions; options: "periodic", "reflective", "open"
+boundary_conditions = "periodic"
 
 
 # data structure etc.
@@ -32,14 +46,3 @@ filename = "file.dat"
 path = None
 ## Export data every dt_export steps
 dt_export = 1
-
-
-# Simulation parameters
-## Integrator algorithm used in the simulation; options: "velocity_verlet", "verlet", "euler"
-integrator = "velocity_verlet"
-## Force used in the simulation; options: "lj", "grav"
-force = "lj"
-## Thermostat used in the simulation; options: "none", "berendsen", "nose-hoover"
-thermostat = "none"
-## Simulation box bounds
-box_bounds = ((0, 10), (0, 10), (0, 10))
--- Exercise Sheet 02/code/simulation_tools.py
+++ Exercise Sheet 02/code/simulation_tools.py
@@ -1,2 +1,55 @@
 import numpy as np
 import params
+import tqdm
+
+# TODO: Write integrator, potential, thermostat and reboxing functions
+# filename: str = params.filename,
+# path: str = params.path,
+# selected_properties: list = True,
+# dt_export: int = params.dt_export,
+
+
+def md_simulation(
+    initial_configuration: np.ndarray,
+    properties: dict = params.properties,
+    potential: str = params.potential,
+    integrator: str = params.integrator,
+    dt: int = params.dt,
+    dt_max: int = params.dt_max,
+    box_bounds: tuple = params.box_bounds,
+    boundary_conditions: str = params.boundary_conditions,
+    thermostat: str = params.thermostat,
+    T: float = params.T,
+) -> np.ndarray:
+    """
+    Run a molecular dynamics simulation.
+
+    Parameters:
+        initial_configuration (numpy.ndarray): The initial configuration of the system featuring the defined properties; format: (properties, particles).
+        properties (dict): The featured properties of the particles. Originally taken from params.
+        potential (str): The potential model to use. Originally taken from params. Options: "lj", "gravitational".
+        integrator (str): The integrator to use. Originally taken from params. Options: "velocity_verlet", "verlet", "euler".
+        dt (int): The time step for the simulation. Originally taken from params.
+        dt_max (int): The maximum time step for the simulation. Originally taken from params.
+        box_bounds (tuple): The bounds of the simulation box. Originally taken from params.
+        boundary_conditions (str): The boundary conditions to use. Originally taken from params. Options: "periodic", "reflective", "open".
+        thermostat (str): The thermostat to use. Originally taken from params. Options: "none", "berendsen", "nose-hoover".
+        T (float): The temperature of the system. Originally taken from params.
+
+    Returns:
+        numpy.ndarray: The data of all configurations in the format (timestep, property, particle) (i.e. shape = (dt_max, len(properties), nparticles)).
+    """
+    # initialize the data array to store the simulation results
+    nparticles = initial_configuration.shape[1]
+    data = np.zeros((dt_max, len(properties), nparticles))
+    data[0] = initial_configuration
+    # choose the integrator function based on the input parameter
+    if integrator == "velocity_verlet":
+        integrator_func = velocity_verlet
+    # update positions and velocities based on the chosen integrator for each time step
+    print(f"Starting simulation with {integrator} integrator for {dt_max} steps.")
+    for t in tqdm(range(1, dt_max)):
+        data[t] = integrator_func(data[t - 1], ...)
+        # TODO: Implement reboxing and thermostat functions here
+    print("Simulation completed.")
+    return data
