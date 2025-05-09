5442265d0e5a357c5b4f0f70011e0849f582e598
Elixus1 <jedai81@web.de>
2025-05-09 15:19:39 +0200

    implemented part a

--- Exercise Sheet 02/code/main02.py
+++ Exercise Sheet 02/code/main02.py
@@ -1,23 +1,16 @@
-from simulation_tools import md_simulation
+# from simulation_tools import md_simulation
 from data_io import export_data
-import solar_system
+from part_a import lj_system_initial_configuration, create_geometry
+import numpy as np
 
-initial_configuration = solar_system.solar_system_initial_configuration
 
-data = md_simulation(
-    initial_configuration,
-    potential="gravitational",
-    potential_params=1.488136e-5,
-    dt_max=1000,
-    dt=0.5,
-    integrator="velocity_verlet",
-    boundary_conditions="none",
-)
+data = lj_system_initial_configuration
+data = create_geometry(data)
 
 export_data(
-    data,
-    selected_properties=["pID", "type", "x", "y", "z", "vx", "vy", "vz"],
-    dt_export=10,
-    filename="output.dat",
-    box_bounds=((-10, 10), (-10, 10), (-10, 10)),
+    np.array([data]),
+    selected_properties=True,
+    dt_export=1,
+    filename="initial_test.dat",
+    box_bounds=((0, 10), (0, 10), (0, 0)),
 )
--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 # Excersice Sheet 01
 
 Wir sollen das Sonnensystem simulieren.
+
+# Exercise Sheet 02
+
+Wir sollen ein LJ fluid simulieren.
