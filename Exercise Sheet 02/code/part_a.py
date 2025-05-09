191c9c64faa93701c00cb9df35943d2e328e55e5
Elixus1 <jedai81@web.de>
2025-05-09 13:46:49 +0200

    first commit

---
+++ Exercise Sheet 02/code/part_a.py
@@ -0,0 +1,8 @@
+"""
+Author: Elias Jedam & Matthias Müller
+Date: 09.05.2025
+
+This file generates the initial conditions for the LJ fluid
+"""
+import numpy as np
+lj_system_initial_configuration = np.array()
