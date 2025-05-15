f4193c21bf1b3c20ac422aff8be15836c852ed29
mmueller1369 <mm1369@email.uni-freiburg.de>
2025-05-15 14:20:41 +0200

    misc 2D -> 3D

--- Exercise Sheet 03/Code/misc.py
+++ Exercise Sheet 03/Code/misc.py
@@ -9,24 +9,20 @@ def WriteEnergy(fileenergy, itime, epot, ekin, vx2, vy2):
     fileenergy.write("%i %e %e %e %e\n" % (itime, epot, ekin, vx2, vy2))
 
 
-def WriteTrajectory(fileoutput, itime, x, y):
+def WriteTrajectory(fileoutput, itime, x, y, z):
 
     fileoutput.write("ITEM: TIMESTEP \n")
     fileoutput.write("%i \n" % itime)
     fileoutput.write("ITEM: NUMBER OF ATOMS \n")
-    fileoutput.write("%i \n" % (settings.n1 * settings.n2))
+    fileoutput.write("%i \n" % (settings.n1 * settings.n2 * settings.n3))
     fileoutput.write("ITEM: BOX BOUNDS \n")
-    fileoutput.write("%e %e xlo xhi \n" % (settings.xlo, settings.xhi))
-    fileoutput.write("%e %e xlo xhi \n" % (settings.ylo, settings.yhi))
-    fileoutput.write("%e %e xlo xhi \n" % (-1, 1))
+    fileoutput.write("%e %e \n" % (settings.xlo, settings.xhi))
+    fileoutput.write("%e %e \n" % (settings.ylo, settings.yhi))
+    fileoutput.write("%e %e \n" % (settings.zlo, settings.zhi))
     fileoutput.write("ITEM: ATOMS id type x y z \n")
 
     for i in range(0, len(x)):
-        z = 0
-        fileoutput.write(
-            "%i %i %e %e %e \n"
-            % (i, i, (x[i] % settings.xhi), (y[i] % settings.yhi), z)
-        )
+        fileoutput.write("%i %i %e %e %e \n" % (i, i, x[i], y[i], z[i]))
 
 
 def inputset():
@@ -35,6 +31,8 @@ def inputset():
         settings.xhi,
         settings.ylo,
         settings.yhi,
+        settings.zlo,
+        settings.zhi,
         settings.eps,
         settings.sigma,
         settings.cutoff,
@@ -43,11 +41,13 @@ def inputset():
     )
 
 
-def squarevelocity(vx, vy, mass):
+def squarevelocity(vx, vy, vz, mass):
     vx2 = 0
     vy2 = 0
+    vz2 = 0
     i = 0
     for i in range(0, len(vx)):
         vx2 += vx[i] ** 2
         vy2 += vy[i] ** 2
-    return 0.5 * mass * vx2, 0.5 * mass * vy2
+        vz2 += vz[i] ** 2
+    return 0.5 * mass * vx2, 0.5 * mass * vy2, 0.5 * mass * vz2
