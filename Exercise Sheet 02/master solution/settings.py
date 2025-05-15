33c8dae4283c28db69a269eb53b6c841c68f1f4e
mmueller1369 <mm1369@email.uni-freiburg.de>
2025-05-15 13:44:31 +0200

    nix sinnvolles

--- Exercise Sheet 02/master solution/settings.py
+++ Exercise Sheet 02/master solution/settings.py
@@ -1,4 +1,4 @@
-#settings
+# settings
 
 # velocity: m*s^-1
 # position: m
@@ -8,25 +8,26 @@
 
 import numpy as np
 
+
 def init():
-    
-    global nsteps            # number of time step to analyze
+
+    global nsteps  # number of time step to analyze
     nsteps = 10000
-    global mass              # mass of the LJ particles (gram/mole)
+    global mass  # mass of the LJ particles (gram/mole)
     mass = 39.95
-    global kb                # boltzmann's constant (kcal/mole/K) 
-    kb =0.0019858775 
-    global Tdesired          # temperature of the experiment in K
-    Tdesired = 300.
-    global eps               # eps in LJ (kcal/mole)
+    global kb  # boltzmann's constant (kcal/mole/K)
+    kb = 0.0019858775
+    global Tdesired  # temperature of the experiment in K
+    Tdesired = 300.0
+    global eps  # eps in LJ (kcal/mole)
     eps = 0.29788162
-    global r0                # r0 in LJ (nm)
+    global r0  # r0 in LJ (nm)
     r0 = 0.188
-    global cutoff            # cutoff arbitrary at 2.5 r0
-    cutoff = 2.5*r0
-    global deltat            # time step (fs)
+    global cutoff  # cutoff arbitrary at 2.5 r0
+    cutoff = 2.5 * r0
+    global deltat  # time step (fs)
     deltat = 1
-    
+
     # number of particle = n1*n2 distributed on s square lattice
     global n1
     n1 = 10
@@ -34,26 +35,22 @@ def init():
     n2 = 10
 
     # desired density
-    rho = 0.25 # / \sigma^2
-    l = np.sqrt(n1*n1/rho)
+    rho = 0.25  # / \sigma^2
+    l = np.sqrt(n1 * n1 / rho)
 
     # box size
     global xlo
-    xlo = 0*r0
+    xlo = 0 * r0
     global xhi
-    xhi = l*r0
+    xhi = l * r0
     global ylo
-    ylo = 0*r0
+    ylo = 0 * r0
     global yhi
-    yhi = l*r0
-    
-    global deltaxyz # lattice parameter to setup the initial configuration on a lattice
-    deltaxyz = (xhi - xlo)/n1
-    
-    #rescaling of temperature
-    global Trescale
-    Trescale = 1 #1 = rescale temperature; 0 = no rescaling
-    
-    
+    yhi = l * r0
 
-    
+    global deltaxyz  # lattice parameter to setup the initial configuration on a lattice
+    deltaxyz = (xhi - xlo) / n1
+
+    # rescaling of temperature
+    global Trescale
+    Trescale = 1  # 1 = rescale temperature; 0 = no rescaling
