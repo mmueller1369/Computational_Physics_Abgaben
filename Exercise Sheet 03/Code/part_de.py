ab33b9d8e837a8f0f3b7c19467ee69f3eaaa3541
mmueller1369 <mm1369@email.uni-freiburg.de>
2025-05-16 18:45:13 +0200

    part d works

--- Exercise Sheet 03/Code/part_de.py
+++ Exercise Sheet 03/Code/part_de.py
@@ -9,8 +9,12 @@ settings.init()
 def potentialLJ(r):
     sf6a = (settings.sigma / settings.cutoff) ** 6
     epotcut = 8.0 * settings.eps * sf6a * (sf6a - 1.0)
-    sf6 = (settings.sigma / r) ** 6
-    epot = 8.0 * settings.eps * sf6 * (sf6 - 1.0) - epotcut
+    sf6 = np.where(r < settings.cutoff, (settings.sigma / r) ** 6, 0.0)
+    epot = np.where(
+        r < settings.cutoff,
+        8.0 * settings.eps * sf6 * (sf6 - 1.0) - epotcut,
+        0,
+    )
     epot[0] = 0.0
     return epot
 
@@ -20,17 +24,15 @@ r = np.loadtxt(os.path.join(settings.path, "r.txt"))  # in nm
 # r = np.linspace(0.15, 1, 100)
 g_r = np.loadtxt(os.path.join(settings.path, "g_r.txt"))
 pot_r = potentialLJ(r)  # in kcal/mole
-integral_core = g_r * pot_r * 4 * np.pi * r**2
 
-# plt.plot(r / settings.sigma, integral_core)
-# plt.plot(r / settings.sigma, pot_r)
-# plt.plot(r / settings.sigma, g_r)
-# plt.ylim(-1, 1)
-# plt.show()
-
+integral_core = (
+    g_r * pot_r * 4 * np.pi * r**2
+)  # over 3d space thus the volume element of the sphere
 rho = settings.rho / settings.sigma**3  # in nm^-3
 integral = rho / 2 * np.sum(integral_core) * settings.deltar
-print(integral)
+print(integral * settings.n1 * settings.n2 * settings.n3)
+
+
 # integral = rho / 2 * np.sum(integral_core[2:] * (settings.deltar / settings.sigma))
 # print(r, pot_r)
 # print(integral)
