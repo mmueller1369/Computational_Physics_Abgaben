8eeb9c914e34fd2c42df42ce8d25926dbb37df9d
mmueller1369 <mm1369@email.uni-freiburg.de>
2025-05-21 15:38:38 +0200

    saved to sheet 4 instead of 3; now corrected
    factor 8 in LJ force -> 4

--- Exercise Sheet 03/Code/settings.py
+++ Exercise Sheet 03/Code/settings.py
@@ -13,60 +13,50 @@ import os
 def init():
 
     global nsteps_equi  # number of time step to analyze
-    nsteps_equi = 50000
+    nsteps_equi = 10000
     global nsteps_production
-    nsteps_production = 100000
+    nsteps_production = 20000
     global mass  # mass of the LJ particles (gram/mole)
     mass = 39.95
     global kb  # boltzmann's constant (kcal/mole/K)
-    kb = 0.0019849421
+    kb = 0.0019858775
     global Tdesired  # temperature of the experiment in K
     Tdesired = 300.0
     global eps  # eps in LJ (kcal/mole)
-    eps = 0.297741315
+    eps = 0.29788162
     global sigma  # sigma in LJ (nm)
     sigma = 0.188
     global cutoff  # cutoff arbitrary at 2.5 sigma
     cutoff = 2.5 * sigma
     global deltat  # time step (fs)
-    deltat = 2
-    global eps_wall  # eps for wall (kcal/mole)
-    eps_wall = 1.4887
-    global sigma_wall  # sigma for wall (nm)
-    sigma_wall = 0.0376
-    global cutoff_wall  # cutoff for wall (nm)
-    cutoff_wall = 2.5 * sigma_wall
+    deltat = 1
 
     # number of particle = n1*n2 distributed on s square lattice
     global n1
-    n1 = 6
+    n1 = 8
     global n2
-    n2 = 6
+    n2 = 8
     global n3
-    n3 = 12
+    n3 = 8
 
     # desired density
     global rho
-    rho = 0.01  # N/V = 0.01 sigma^-3
-
-    # box lengths in each direction
-    lx = n1 / (rho ** (1 / 3))
-    ly = n2 / (rho ** (1 / 3))
-    lz = n3 / (rho ** (1 / 3))
+    rho = 0.5  # N/V = 0.01 sigma^-3
+    l = (n1 * n2 * n3 / rho) ** (1 / 3)  # l = (N/V)^(1/3) = (n1*n2*n3/rho)^(1/3)
 
     # box size
     global xlo
     xlo = 0 * sigma
     global xhi
-    xhi = lx * sigma
+    xhi = l * sigma
     global ylo
     ylo = 0 * sigma
     global yhi
-    yhi = ly * sigma
+    yhi = l * sigma
     global zlo
     zlo = 0 * sigma
     global zhi
-    zhi = lz * sigma
+    zhi = l * sigma
 
     global deltaxyz  # lattice parameter to setup the initial configuration on a lattice
     deltaxyz = (xhi - xlo) / n1
@@ -78,7 +68,7 @@ def init():
     global deltar  # bin size for histogram
     deltar = 0.01 * sigma
     global rmax  # maximum distance for histogram
-    rmax = 1 / 2 * max(lx, ly, lz) * sigma  # should be 1/2 of the box size
+    rmax = 1 / 2 * l * sigma  # should be 1/2 of the box size
     global n_analyze  # every n_analyze steps, the histogram is calculated
     n_analyze = 10
     global n_gr
