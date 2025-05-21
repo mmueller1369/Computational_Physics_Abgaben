2075678dade7986b8bfd8a193439c44d6b4041e1
mmueller1369 <mm1369@email.uni-freiburg.de>
2025-05-21 09:26:49 +0200

    added sheet 04

---
+++ Ecercise Sheet 04/Code/g_r.py
@@ -0,0 +1,97 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import settings
+import os
+from force import pbc
+from numba import njit, prange
+import os
+
+
+@njit
+def histogram(x, y, z, bin_width, rmax):
+    # calc the distance between the particles
+    # only consider distances less than rmax
+    hist = np.zeros(int(rmax / bin_width))
+    for i in prange(len(x)):
+        for j in prange(i + 1, len(x)):
+            # rijx = x[i] - x[j]
+            # rijy = y[i] - y[j]
+            # rijz = z[i] - z[j]
+            rijx = pbc(x[i], x[j], settings.xlo, settings.xhi)
+            rijy = pbc(y[i], y[j], settings.ylo, settings.yhi)
+            rijz = pbc(z[i], z[j], settings.zlo, settings.zhi)
+
+            r2 = rijx * rijx + rijy * rijy + rijz * rijz
+            if r2 < rmax * rmax:
+                r = np.sqrt(r2)
+                bin_n = int(r / bin_width)
+                hist[bin_n] += 2
+    # after sort the distance in the bins
+
+    return hist
+
+
+@njit
+def calc_RDF(histogram, bin_width):
+    # calculate the average number of atoms in each bin
+    total_bins = histogram.shape[1]
+    nparticles = settings.n1 * settings.n2 * settings.n3
+    histogram_new = np.zeros(total_bins)
+    for i in prange(total_bins):
+        total_atoms = 0
+        for j in prange(histogram.shape[0]):
+            total_atoms += histogram[j][i]
+        histogram_new[i] = total_atoms / settings.n_gr / nparticles
+
+    # calculate the n(b) idela gas
+    histogram_ideal = np.zeros(total_bins)
+    rho = settings.rho / settings.sigma**3
+    for i in prange(total_bins):
+        histogram_ideal[i] = (
+            4
+            / 3
+            * np.pi
+            * rho
+            * ((i * bin_width + bin_width) ** 3 - (i * bin_width) ** 3)
+        )
+    return histogram_new / histogram_ideal, [histogram_new, histogram_ideal]
+
+
+def plot_histogram(hist):
+    # plot the histogram
+    plt.plot(hist)
+    plt.xlabel("r (nm)")
+    plt.ylabel("g(r)")
+    plt.title("test")
+    plt.show()
+
+    total_sum = np.sum(hist)
+    print("Sum of all bins:", total_sum)
+
+
+def plot_rdf(rdf, bin_width):
+    x = np.arange(0, len(rdf)) * bin_width / settings.sigma
+    # box_length_sigma = settings.xhi / settings.sigma / 2
+    # other_line = np.sqrt(2) * box_length_sigma
+    # plt.axvline(other_line, color="g", linestyle="--", label="sqrt(2) * box length")
+    # plt.axvline(box_length_sigma, color="r", linestyle="--", label="box length/2")
+    plt.title("Radial Distribution Function LJ Potential")
+    plt.axhline(1, color="y", linestyle="--", label="Asymptote")
+    plt.plot(x, rdf, label="g(r)")
+    plt.xlabel("r/ sigma")
+    plt.ylabel("g(r)")
+    plt.legend()
+    plt.savefig(os.path.join(settings.path, "g_r.png"))
+    plt.show()
+    # evaluate the plot
+    # find max peak
+    max_id = np.argmax(rdf)
+    print("Max value in RDF is: ", rdf[max_id])
+    print("The value is at position: ", max_id * bin_width / settings.sigma, " r/sigma")
+    min_id_rel = np.argmin(rdf[max_id:])
+    min_id = max_id + min_id_rel
+    print("Next minimum after max is: ", rdf[min_id])
+    print("The value is at position: ", min_id * bin_width / settings.sigma, " r/sigma")
+
+    np.savetxt(os.path.join(settings.path, "g_r.txt"), rdf)
+    np.savetxt(os.path.join(settings.path, "r.txt"), x * settings.sigma)
---
+++ Ecercise Sheet 04/Code/settings.py
@@ -0,0 +1,78 @@
+# settings
+
+# velocity: m*s^-1
+# position: m
+# acceleration: ms^-2
+# energy: joule
+# temperature: K
+
+import numpy as np
+import os
+
+
+def init():
+
+    global nsteps_equi  # number of time step to analyze
+    nsteps_equi = 10000
+    global nsteps_production
+    nsteps_production = 20000
+    global mass  # mass of the LJ particles (gram/mole)
+    mass = 39.95
+    global kb  # boltzmann's constant (kcal/mole/K)
+    kb = 0.0019858775
+    global Tdesired  # temperature of the experiment in K
+    Tdesired = 300.0
+    global eps  # eps in LJ (kcal/mole)
+    eps = 0.29788162
+    global sigma  # sigma in LJ (nm)
+    sigma = 0.188
+    global cutoff  # cutoff arbitrary at 2.5 sigma
+    cutoff = 2.5 * sigma
+    global deltat  # time step (fs)
+    deltat = 1
+
+    # number of particle = n1*n2 distributed on s square lattice
+    global n1
+    n1 = 8
+    global n2
+    n2 = 8
+    global n3
+    n3 = 8
+
+    # desired density
+    global rho
+    rho = 0.5  # N/V = 0.01 sigma^-3
+    l = (n1 * n2 * n3 / rho) ** (1 / 3)  # l = (N/V)^(1/3) = (n1*n2*n3/rho)^(1/3)
+
+    # box size
+    global xlo
+    xlo = 0 * sigma
+    global xhi
+    xhi = l * sigma
+    global ylo
+    ylo = 0 * sigma
+    global yhi
+    yhi = l * sigma
+    global zlo
+    zlo = 0 * sigma
+    global zhi
+    zhi = l * sigma
+
+    global deltaxyz  # lattice parameter to setup the initial configuration on a lattice
+    deltaxyz = (xhi - xlo) / n1
+
+    # rescaling of temperature
+    global Trescale
+    Trescale = 1  # 1 = rescale temperature; 0 = no rescaling
+
+    global deltar  # bin size for histogram
+    deltar = 0.01 * sigma
+    global rmax  # maximum distance for histogram
+    rmax = 1 / 2 * l * sigma  # should be 1/2 of the box size
+    global n_analyze  # every n_analyze steps, the histogram is calculated
+    n_analyze = 10
+    global n_gr
+    n_gr = int(nsteps_production / n_analyze)
+
+    global path  # path to the output files
+    path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "output")
