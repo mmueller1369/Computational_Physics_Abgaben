e533ed3e513c37421e262977e16baf5c3f4c26be
mmueller1369 <mm1369@email.uni-freiburg.de>
2025-06-14 21:13:00 +0200

    part b complete

--- Exercise Sheet 06/Code/forces.py
+++ Exercise Sheet 06/Code/forces.py
@@ -16,6 +16,41 @@ def forceLJ(x, y, z, xlo, xhi, ylo, yhi, zlo, zhi, eps, sigma, cutoff):
     N = len(x)
 
     i = 0
+    epot = 0
+
+    for i in prange(N - 1):
+        j = i + 1
+        for j in prange(i + 1, N):
+            rijx = pbc(x[i], x[j], xlo, xhi)
+            rijy = pbc(y[i], y[j], ylo, yhi)
+            rijz = pbc(z[i], z[j], zlo, zhi)
+
+            r2 = rijx * rijx + rijy * rijy + rijz * rijz
+            # calculate fx, fy, fz
+            sf2 = sigma * sigma / r2
+            sf6 = sf2 * sf2 * sf2
+            epot += 4.0 * eps * sf6 * (sf6 - 1.0)
+            ff = 24.0 * eps * sf6 * (sf6 - 0.5) / r2
+            fx[i] -= ff * rijx
+            fy[i] -= ff * rijy
+            fz[i] -= ff * rijz
+
+            fx[j] += ff * rijx
+            fy[j] += ff * rijy
+            fz[j] += ff * rijz
+
+    return fx, fy, fz, epot
+
+
+@njit(parallel=True)
+def forceLJ_cut(x, y, z, xlo, xhi, ylo, yhi, zlo, zhi, eps, sigma, cutoff):
+
+    fx = np.zeros(shape=len(x))
+    fy = np.zeros(shape=len(x))
+    fz = np.zeros(shape=len(x))
+    N = len(x)
+
+    i = 0
     sf2a = sigma * sigma / cutoff / cutoff
     sf6a = sf2a * sf2a * sf2a
 
--- Exercise Sheet 06/Code/main.py
+++ Exercise Sheet 06/Code/main.py
@@ -3,28 +3,28 @@ import settings
 import initialize
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
 settings.init()
 x, y, z, vx, vy, vz = initialize.InitializeAtoms()
 f_initial = np.zeros(shape=(settings.n1 * settings.n2 * settings.n3))
 initial_config = [x, y, z, vx, vy, vz, f_initial, f_initial, f_initial]
 
-"""
+
 # ----------------- Part a ----------------- #
-def B2(beta, eps, sigma, cutoff):
-    def potentialLJ(r, eps, sigma, cutoff):
-        sf6a = (sigma / cutoff) ** 6
-        epotcut = 4.0 * eps * sf6a * (sf6a - 1.0)
-        mask = (r < cutoff) & (r > 0)
+def B2(beta, eps, sigma):
+    def potentialLJ(r, eps, sigma):
+        mask = r > 0
         sf6 = np.zeros_like(r)
         sf6[mask] = (sigma / r[mask]) ** 6
         epot = np.zeros_like(r)
-        epot[mask] = 4.0 * eps * sf6[mask] * (sf6[mask] - 1.0) - epotcut
+        epot[mask] = 4.0 * eps * sf6[mask] * (sf6[mask] - 1.0)
         return epot
 
-    deltar = cutoff / 1000
+    cutoff = (settings.xhi - settings.xlo) * 3
+    deltar = cutoff / 10000
     r = np.arange(0, cutoff, deltar)
-    U = potentialLJ(r, eps, sigma, cutoff)
+    U = potentialLJ(r, eps, sigma)
     integral_core = np.exp(-beta * U) - 1
     integral_core *= 4 * np.pi * r**2  # conversion into spherical coordinates
     integral = -1 / 2 * np.sum(integral_core) * deltar
@@ -32,27 +32,27 @@ def B2(beta, eps, sigma, cutoff):
 
 
 beta = 1 / (settings.Tdesired * settings.kb)
-sigma, cutoff = settings.sigma, 2.5 * settings.sigma
+sigma = settings.sigma
 print(
-    "For our parameter set: B2/eps =",
-    B2(
+    f"a: for our parameter set (argon): B2/eps = {B2(
         beta,
         1,  # set to unity
         sigma,
-        cutoff,
-    ),
+    ):.4f}"
 )
 
 epss = np.linspace(0, 1, 1000) / beta
-B2_eps = [B2(beta, eps, sigma, cutoff) / sigma**3 for eps in epss]
+B2_eps = [B2(beta, eps, sigma) / sigma**3 for eps in epss]
 
-plt.figure()
-plt.plot(epss * beta, B2_eps)
+plt.figure(figsize=(6, 4))
+plt.plot(epss * beta, B2_eps, label=r"$B_2/\sigma^3$", color="b")
+plt.axvline(settings.eps * beta, label=r"$\epsilon\beta_\mathrm{Argon}$", color="r")
 plt.xlabel(r"$\epsilon\beta$")
 plt.ylabel(r"$B_2/\sigma^3$")
-plt.grid(True)
+plt.grid()
+plt.legend()
+plt.savefig(os.path.join(settings.path, "a.png"), bbox_inches="tight", dpi=300)
 plt.show()
-"""
 
 
 # ----------------- Part b ----------------- #
@@ -89,154 +89,51 @@ final_config = execute.run_simulation(
     n_save=10,
     simulation_name="Production part b",
 )
-"""
-# ----------------- Part a ----------------- #
-settings.tau = 5000 * settings.deltat
-settings.Tdesired = 300
-settings.eps = 0.5 * settings.kb * settings.Tdesired
-config_1 = execute.run_simulation(
-    initial_config=initial_config,
-    integrator="VelocityVerlet",
-    force="LJ",
-    steps=40000,
-    thermostat="berendsen_thermostat",
-    thermostat_params=[settings.Tdesired, settings.tau, settings.deltat],
-    n_thermostat=1,
-    trajfile=False,
-    tempfile="temp_a_1",
-    energyfile=False,
-    rdffile=False,
-    n_save=10,
-    simulation_name="T=300K",
+
+b_temp = np.loadtxt(os.path.join(settings.path, "b_temp.txt"))[:, 1]
+b_press = np.loadtxt(os.path.join(settings.path, "b_press.txt"))[:, 1]
+b_r, b_gr = np.loadtxt(os.path.join(settings.path, "b_gr.txt")).T
+t = np.arange(len(b_temp)) * 2
+
+fig, ax1 = plt.subplots(figsize=(7, 4))
+ax1.set_xlabel(r"$t$ [fs]")
+ax1.set_ylabel(r"$T$ [K]", color="b")
+ax1.plot(t, b_temp, color="b", label="Temperature")
+ax1.tick_params(axis="y", labelcolor="b")
+ax2 = ax1.twinx()
+ax2.set_ylabel(
+    r"$P$ [$\mathrm{g/mole}\cdot\mathrm{nm}^{-1}\cdot\mathrm{fs}^{-2}$]", color="r"
 )
+ax2.plot(t, b_press, color="r", label="Pressure")
+ax2.tick_params(axis="y", labelcolor="r")
+fig.tight_layout()
+plt.savefig(os.path.join(settings.path, "b_PT.png"), bbox_inches="tight", dpi=300)
+plt.show()
 
-settings.Tdesired = 100
-settings.eps = 0.5 * settings.kb * settings.Tdesired
-config_2 = execute.run_simulation(
-    initial_config=config_1,
-    integrator="VelocityVerlet",
-    force="LJ",
-    steps=40000,
-    thermostat="berendsen_thermostat",
-    thermostat_params=[settings.Tdesired, settings.tau, settings.deltat],
-    n_thermostat=1,
-    trajfile=False,
-    tempfile="temp_a_2",
-    energyfile=False,
-    rdffile=False,
-    n_save=10,
-    simulation_name="T=100K",
-)
+plt.figure(figsize=(7, 5))
+plt.plot(b_r, b_gr, color="b")
+plt.xlabel(r"$r / \sigma$")
+plt.ylabel(r"$g(r)$")
+plt.grid()
+plt.savefig(os.path.join(settings.path, "b_g.png"), bbox_inches="tight", dpi=300)
+plt.show()
 
-
-# ----------------- Part b ----------------- #
-settings.tau = 5000 * settings.deltat
-settings.Tdesired = 300
-settings.eps = 0.5 * settings.kb * settings.Tdesired
-config_1 = execute.run_simulation(
-    initial_config=initial_config,
-    integrator="VelocityVerlet",
-    force="LJ",
-    steps=40000,
-    thermostat="andersen_thermostat",
-    thermostat_params=[settings.Tdesired, settings.nu, settings.deltat],
-    n_thermostat=1,
-    trajfile=False,
-    tempfile="temp_b_1",
-    energyfile=False,
-    rdffile=False,
-    n_save=10,
-    simulation_name="T=300K",
-)
-
-settings.Tdesired = 100
-settings.eps = 0.5 * settings.kb * settings.Tdesired
-config_2 = execute.run_simulation(
-    initial_config=config_1,
-    integrator="VelocityVerlet",
-    force="LJ",
-    steps=40000,
-    thermostat="andersen_thermostat",
-    thermostat_params=[settings.Tdesired, settings.nu, settings.deltat],
-    n_thermostat=1,
-    trajfile=False,
-    tempfile="temp_b_2",
-    energyfile=False,
-    rdffile=False,
-    n_save=10,
-    simulation_name="T=100K",
-)
-
-
-# ----------------- Part c+d ----------------- #
-settings.tau = 500 * settings.deltat
-settings.Tdesired = 300
-settings.eps = 0.5 * settings.kb * settings.Tdesired
-equilibrated_config = execute.run_simulation(
-    initial_config=initial_config,
-    integrator="VelocityVerlet",
-    force="LJ",
-    steps=40000,
-    thermostat="berendsen_thermostat",
-    thermostat_params=[settings.Tdesired, settings.tau, settings.deltat],
-    n_thermostat=1,
-    trajfile=False,
-    tempfile=False,
-    energyfile=False,
-    rdffile=False,
-    n_save=10,
-    simulation_name="Equilibration",
-)
-
-final_config = execute.run_simulation(
-    initial_config=equilibrated_config,
-    integrator="VelocityVerlet",
-    force="LJ",
-    steps=40000,
-    thermostat="berendsen_thermostat",
-    thermostat_params=[settings.Tdesired, settings.tau, settings.deltat],
-    n_thermostat=1,
-    trajfile=False,
-    tempfile="temp_cd",
-    energyfile="energy_cd",
-    rdffile="rdf_cd",
-    n_save=10,
-    simulation_name="Production",
-)
-
-# ----------------- Part e ----------------- #
-settings.tau = 500 * settings.deltat
-settings.Tdesired = 300
-settings.eps = 0.5 * settings.kb * settings.Tdesired
-equilibrated_config = execute.run_simulation(
-    initial_config=initial_config,
-    integrator="VelocityVerlet",
-    force="LJ",
-    steps=40000,
-    thermostat="andersen_thermostat",
-    thermostat_params=[settings.Tdesired, settings.nu, settings.deltat],
-    n_thermostat=1,
-    trajfile=False,
-    tempfile=False,
-    energyfile=False,
-    rdffile=False,
-    n_save=10,
-    simulation_name="Equilibration",
-)
-
-final_config = execute.run_simulation(
-    initial_config=equilibrated_config,
-    integrator="VelocityVerlet",
-    force="LJ",
-    steps=40000,
-    thermostat="andersen_thermostat",
-    thermostat_params=[settings.Tdesired, settings.nu, settings.deltat],
-    n_thermostat=1,
-    trajfile=False,
-    tempfile="temp_e",
-    energyfile="energy_e",
-    rdffile="rdf_e",
-    n_save=10,
-    simulation_name="Production",
-)
-"""
+num_blocks = 5
+block_size = len(b_press) // num_blocks
+block_means = np.zeros(num_blocks)
+for i in range(num_blocks):
+    start = i * block_size
+    if i < num_blocks - 1:
+        end = (i + 1) * block_size
+    else:
+        end = len(b_press)
+    block_means[i] = np.mean(b_press[start:end])
+print(f"b: iii: mean values for each block: {block_means}")
+P_mean = np.mean(block_means)
+P_error = np.std(block_means, ddof=1) / np.sqrt(num_blocks)
+print(f"b: iv: P_mean: {P_mean:.4f}")
+print(f"b: iv: P_error: {P_error:.4f}")
+rho = settings.rho / sigma**3
+B2_sys = B2(beta, settings.eps, sigma)
+P_B2 = 1 / beta * rho + 1 / beta * B2_sys * rho**2
+print(f"b: v: P_B2: {P_B2:.4f}")
